09536d461145b16d4f2dce202e42bdef5b425cbc
jobairshakib <you@example.comshakib.jobair@gmail.com>
2022-05-26 21:39:13 +0600

    admin role checked

--- index.js
+++ index.js
@@ -42,14 +42,21 @@ async function run() {
             res.send(users);
         });
 
-        app.put('/user/admin/:email', async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: {role:'admin'},
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: {role:'admin'},
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                return res.send(result);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            }
         });
 
         app.put('/user/:email', async (req, res) => {

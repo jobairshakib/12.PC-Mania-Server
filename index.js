a3d520da8ccb0b06626e2bff5c47d5ee788048ae
jobairshakib <you@example.comshakib.jobair@gmail.com>
2022-05-26 12:59:41 +0600

    purchases added for all item

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const partCollection = client.db("PC_Mania").collection("parts");
+        const purchaseCollection = client.db("PC_Mania").collection("purchases");
 
         app.get("/part", async (req, res) => {
             const query = {};
@@ -32,7 +33,47 @@ async function run() {
             const part = await partCollection.findOne(query);
             res.send(part);
         });
+
+        // Purchases
+        app.get('/purchases', async (req, res) => {
+            const query = {};
+            const result = await purchaseCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/purchase', async (req, res) => {
+            const customer = req.query.customer;
+            const query = { customer: customer };
+            const result = await purchaseCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post("/purchase", async (req, res) => {
+            const purchase = req.body;
+            const result = await purchaseCollection.insertOne(purchase);
+            res.send(result);
+        });
         
+        app.delete('/purchase/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await purchaseCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        
+
+        app.put("/part/:id", async (req, res) => {
+            const id = req.params.id;
+            const updateStock = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = { $set: updateStock };
+            const result = await partCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+        
+
     } finally {
     }
 }

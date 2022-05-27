7bdcde2c768d5ee7e206bc50b520a229115df189
jobairshakib <shakib.jobair@gmail.com>
2022-05-27 23:18:13 +0600

    payment post added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -126,6 +128,13 @@ async function run() {
             }
         })
 
+        app.get('/purchase/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const purchase = await purchaseCollection.findOne(query);
+            res.send(purchase);
+        })
+
         app.post("/purchase", async (req, res) => {
             const purchase = req.body;
             const result = await purchaseCollection.insertOne(purchase);
@@ -163,6 +172,21 @@ async function run() {
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
+
+        // Payment
+        app.post('/create-payment-intent', verifyJWT, async(req, res)=> {
+            const purchase = req.body;
+            const price = purchase.totalPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "bdt",
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        });
 
     } finally {
     }
